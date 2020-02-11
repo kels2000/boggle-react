05a6ca29fbda1de1a0a0ccaefab24c0f615efc49
kels2000 <46412797+kels2000@users.noreply.github.com>
2020-02-11 11:40:44 -0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,6 +5,12 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+{"firestore": {
+    "rules": "firestore.rules",
+    "indexes": "firestore.indexes.json"
+  }
+}
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
